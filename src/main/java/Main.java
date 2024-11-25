c8e9a8e8ddb5f714db9edb5bc36e795b1a15d601
Wes Bovee <wesbovee@Wess-MacBook-Pro-2.local>
2024-11-24 22:52:51 -0700

    add main function

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,55 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Testing Dockerfile");
+        System.out.println("welcome to the calculator\n");
+        System.out.println("Enter a command:");
+        Calculator calculator = new Calculator();
+        int result;
+        Scanner scanner = new Scanner(System.in);
+
+        boolean exit = false;
+        while(!exit) {
+            if (scanner.hasNextLine()) {
+                String input = scanner.nextLine();
+                String[] parts = input.split(" ");
+                String command = parts[0];
+                switch (command) {
+                    case "add":
+                        result = calculator.add(Integer.parseInt(parts[1]), Integer.parseInt(parts[2]));
+                        System.out.println(result);
+                        break;
+
+                    case "subtract":
+                        result = calculator.subtract(Integer.parseInt(parts[1]), Integer.parseInt(parts[2]));
+                        System.out.println(result);
+                        break;
+
+                    case "multiply":
+                        result = calculator.multiply(Integer.parseInt(parts[1]), Integer.parseInt(parts[2]));
+                        System.out.println(result);
+                        break;
+
+                    case "divide":
+                        result = calculator.divide(Integer.parseInt(parts[1]), Integer.parseInt(parts[2]));
+                        System.out.println(result);
+                        break;
+
+                    case "fibonacci":
+                        result = calculator.fibonacciNumberFinder(Integer.parseInt(parts[1]));
+                        System.out.println(result);
+                        break;
+
+                    case "binary":
+                        String binary = calculator.intToBinaryNumber(Integer.parseInt(parts[1]));
+                        System.out.println(binary);
+                        break;
+                    case "exit":
+                        exit = true;
+                        break;
+                }
+            }
+        }
+        scanner.close();
     }
 }
